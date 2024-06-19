ef2af1aa7ddf2d975372944ced012c6b9fa49e76
Salq24 <qogheneochuko@gmail.com>
2024-06-19 10:00:40 -0700

    api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""function that queries the reddit api and returns the
+num of sbscribers"""
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """ the function that queries reddit api"""
+    headers = {'User-agent': 'Chrome/58.0.3029.110 Safari/537.3'}
+    link = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = get(link, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
+
+
